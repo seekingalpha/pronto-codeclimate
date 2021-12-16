2f346fff9ec6bd211ef2bf3aec51606ee67cdf85
Alex Tatarnikov <oleksandr@seekingalpha.com>
2021-12-16 22:15:14 +0200

    Customize cop message

--- lib/pronto/codeclimate/issue.rb
+++ lib/pronto/codeclimate/issue.rb
@@ -3,6 +3,14 @@
 module Pronto
   class Codeclimate < Runner
     class Issue
+      REMEDIATION_GRADES = {
+        (0..2_000_000) => 'A',
+        (2_000_000..4_000_000) => 'B',
+        (4_000_000..8_000_000) => 'C',
+        (8_000_000..16_000_000) => 'D',
+        (16_000_000..) => 'F',
+      }.freeze
+
       LEVEL_MAPPING = {
         'info' => :info,
         'minor' => :warning,
@@ -24,11 +32,25 @@ module Pronto
       private
 
       def text
-        message_text = @issue['description']
+        message_text = "#{@issue['description']}\n\n"
 
-        return message_text unless @issue.dig('content', 'body')
+        message_text += "\n```ruby\n#{@issue.dig('content', 'body')}\n```" if @issue.dig('content', 'body')
 
-        "#{message_text}\n\n```Suggestion:\n#{@issue.dig('content', 'body')}```"
+        message_text +=
+          "\n\n" \
+          "| #{@issue['engine_name'].capitalize} |                                  |\n"\
+          "| ----------------------------------- | -------------------------------- |\n"\
+          "| Cop                                 | #{@issue['check_name']}          |\n"\
+          "| Complexity                          | [#{remediation_grade}](https://docs.codeclimate.com/docs/code-climate-glossary#section-remediation)             |\n"\
+          "| Severity                            | #{@issue['severity'].capitalize} |\n"
+
+        message_text
+      end
+
+      def remediation_grade
+        REMEDIATION_GRADES.select do |range, grade|
+          range.include?(@issue['remediation_points'])
+        end.values.first
       end
 
       def path
